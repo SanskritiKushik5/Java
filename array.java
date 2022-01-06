e85f8ac2ee41be65d1bbe9819c070416b0da52ae
Sanskriti Kushik <sanskritikushik23@gmail.com>
2022-01-06 12:54:23 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	
+	public static void main (String[] args) {
+	    int[] arr = {2, 4, 6, 8, 10};
+	    int[] array = new int[] {1, 2, 3};
+	    System.out.println(arr[0]);
+	}
+}
