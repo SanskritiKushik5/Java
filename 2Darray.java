f21178a095b457ddb31d3f7de53943ae0071374e
Sanskriti Kushik <sanskritikushik23@gmail.com>
2022-01-06 13:39:19 +0530

    Create 2Darray.java

---
+++ 2Darray.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	
+	public static void main (String[] args) {
+	    int[][] arr = {
+	        {2, 4}, 
+	        {6, 8}
+	    };
+	    System.out.println(arr[0].length);
+	}
+}
